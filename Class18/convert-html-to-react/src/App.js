0fdd3a998995773634dc4a1f49209ef618653580
Utkarshini12 <50311757+Utkarshini12@users.noreply.github.com>
2021-06-01 15:50:41 +0530

    Add files via upload

---
+++ Class18/convert-html-to-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Navbar'
+
+const App = () => {
+  return (
+    <div>
+    <h1>Utkarshini</h1>
+    <NavBar/>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ Class18/tictac/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './App.css';
+import Icons from './components/Icons'
+import {toastContainer, toast} from 'react-toastify'
+
+import {Card, CardBody, Container, Button, Col, Row} from 'reactstrap'
+
+const App =() => {
+  return (
+    <Container className='p-5'
+    <Row>
+    <Col></Col>
+    <Col></Col>
+    <Col></Col>
+
+    </Row>
+
+    
+
+  )
+}
+
+export default App;
