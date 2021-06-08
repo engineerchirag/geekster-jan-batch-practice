c2fc87081b8c5e79d39673bc6bf85e4b6dc7ea1b
Utkarshini12 <50311757+Utkarshini12@users.noreply.github.com>
2021-06-08 17:04:46 +0530

    Class 20

--- Class 19/react/src/App.js
+++ Class 19/react/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import ErrorExample from './project/1-useState/setup/1-error-example'
+import UseStateBasics from './project/1-useState/setup/2-useState-basics'
+import useStateCounter from './project/1-useState/3-useState-Counter'
+import UseStateCounter from './project/1-useState/3-useState-Counter';
 function App() {
   return (
+    <div className='conatiner'>
     <h2>
     Advance React 
     </h2>
+  <div/>
+   <UseStateCounter />
+
+    </div>
     
   )
 }
 
-export default App
+export default App;
