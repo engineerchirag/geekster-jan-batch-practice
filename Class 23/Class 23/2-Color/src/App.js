e276521679a06d8781c38528dca5c529d87796fe
Utkarshini12 <50311757+Utkarshini12@users.noreply.github.com>
2021-06-17 21:26:16 +0530

    Add files via upload

---
+++ Class 23/Class 23/2-Color/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+
+  const [color, setColor] = useState('');
+  const [list, setList] = useState([])
+  return <h2>color generator project</h2>
+}
+
+export default App
